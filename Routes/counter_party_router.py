2d86cf809129a81d770bf68e81a4c78e66a30a1f
Josue Garrido <garrido.jd20@gmail.com>
2025-06-13 17:33:02 -0500

    Commit #4: Cached Token COBRE

--- Routes/counter_party_router.py
+++ Routes/counter_party_router.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 
 from Controllers.counter_party_controller import CounterParty
+from Controllers.cobre_controller import Cobre
 
 counterPartyRoutes = Blueprint('counter_party', __name__)
+
+@counterPartyRoutes.route('/get-token-cobre', methods=['GET'])
+def get_token_cobre():
+    return Cobre().get_cobre_token()
 
 @counterPartyRoutes.route('/list-counterparty', methods=['GET'])
 def get_list_counter_party():
